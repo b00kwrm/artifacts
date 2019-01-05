beb00f7f282c5b937dbf966e482b4528d00f90c9
dave <dave@harpo.marx>
2019-01-05 11:12:58 -0500

    Fixed typo in test docstring. Added a more desrctiption error message to the test that shows the artifact and file that needs updating.

--- tests/mitre_attack_url.py
+++ tests/mitre_attack_url.py
@@ -18,7 +18,7 @@ class MitreAttackURLTest(test_lib.BaseTestCase):
   """Class to test if the mitre attack url is inlcuded in artifact."""
 
   def testForMitreAttackURL(self):
-    """checks for Mitre Attack URL."""
+    """Checks for Mitre Attack URL in artifacts."""
     for artifacts_file in glob.glob(os.path.join('data', '*.yaml')):
         with open(artifacts_file, 'rb') as yml_file:
             yml_file_load = yaml.safe_load_all(yml_file)
@@ -26,8 +26,7 @@ class MitreAttackURLTest(test_lib.BaseTestCase):
             for artifact in my_artifacts:
                 if artifact.get('urls'):
                     for url in artifact.get('urls'):
-                        self.assertIn("attack.mitre.org", url)
-
-
+                        self.assertIn("attack.mitre.org", url, msg='add mitre url to {} in {}.'.format(artifact['name'], artifacts_file))
+                        
 if __name__ == '__main__':
   unittest.main()
